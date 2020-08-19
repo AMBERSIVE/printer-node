3feda59019264d50fd44b96c0eca16e0ca95cfe1
leganz <manuel.pirker-ihl@ambersive.com>
2020-08-19 21:55:54 +0200

    Add support for some query string parameter for GET printer request

--- src/printer/printer.controller.ts
+++ src/printer/printer.controller.ts
@@ -1,14 +1,47 @@
-import { Controller, Get, Post, Body, Res, Req, HttpStatus, Param, UseGuards, UsePipes, ValidationPipe, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, Req, HttpStatus, Param, UseGuards, UsePipes, ValidationPipe, Delete, Query } from '@nestjs/common';
 import { Response, Request } from 'express';
+import { PrinterService } from './printer.service';
+import { time } from 'console';
 
 
 @Controller()
 export class PrinterController {
 
-    @Get("api/printer")
-    async getPrinter(@Param() params, @Req() req: Request, @Res() res: Response) {
+    public allowedQueryStrings:Array<string> = [
+        'active',
+        'isDefault'
+    ];
 
-        let result = {};
+    constructor(private readonly printerService: PrinterService) {
+        
+    }
+
+    @Get("api/printer")
+    async getPrinter(@Param() params, @Req() req: Request, @Res() res: Response, @Query("active") isActive, @Query('isDefault') isDefault) {
+
+        let result = await this.printerService.findAll();
+
+        if (result.length > 0) {
+            
+            if (isActive != undefined) {
+                isActive = isActive == 'true';
+                result = result.filter(item => {
+                    if (item.active === isActive) {
+                        return item;
+                    }
+                });
+            }
+
+            if (isDefault != undefined) {
+                isDefault = isDefault == 'true';
+                result = result.filter(item => {
+                    if (item.isDefault === isDefault) {
+                        return item;
+                    }
+                });
+            }
+
+        }
 
         res.status(HttpStatus.OK).json(result);
 
